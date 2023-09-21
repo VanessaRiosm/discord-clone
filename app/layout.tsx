02055892d9662c1ac13e570e3a592979ebd872fd
Vanessa <vanessa.riosm571@gmail.com>
2023-09-21 12:42:18 -0500

    feat: add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type {Metadata} from 'next'
 import {Open_Sans} from 'next/font/google'
+import {ClerkProvider} from '@clerk/nextjs'
+import {ThemeProvider} from '@/components/providers/theme-provider'
+import {cn} from '@/lib/utils'
 
 const font = Open_Sans({subsets: ['latin']})
 
@@ -11,8 +14,19 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang='en'>
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={false}
+            storageKey='discord-theme'
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
