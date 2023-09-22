e6c0285e1229c9cd56d07151160eb12277e384e6
Vanessa <vanessa.riosm571@gmail.com>
2023-09-22 15:53:36 -0500

    feat: add server icon left navbar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,9 @@ import {currentProfile} from '@/lib/current-profile'
 import {db} from '@/lib/db'
 import {redirect} from 'next/navigation'
 import {NavigationAction} from './navigation-action'
+import {NavigationItem} from './navigation-item'
+import {Separator} from '@/components/ui/separator'
+import {ScrollArea} from '@/components/ui/scroll-area'
 
 export const NavigationSidebar = async () => {
   const profile: any = await currentProfile
@@ -21,6 +24,18 @@ export const NavigationSidebar = async () => {
   return (
     <div className='space-y-4 flex flex-col items-center h-screen text-rpimary w-full dark:bg-[#1E1F22] py-3'>
       <NavigationAction />
+      <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto' />
+      <ScrollArea className='flex-1 w-full'>
+        {servers.map((server) => (
+          <div key={server.id} className='mb-4'>
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   )
 }
