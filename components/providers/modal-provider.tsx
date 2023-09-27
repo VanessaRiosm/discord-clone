a9891df31f74fd8f3fa08ea14032315cbdbc704f
Vanessa <vanessa.riosm571@gmail.com>
2023-09-27 16:01:57 -0500

    feat: add 'invite friend' server option

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import {useEffect, useState} from 'react'
-import {CreateServerModal} from '../modals/create-server-modal'
+import {CreateServerModal} from '@/components/modals/create-server-modal'
+import {InviteModal} from '@/components/modals/invite-modal'
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -17,6 +18,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -18,6 +18,7 @@ import {
   UserPlus,
   Users,
 } from 'lucide-react'
+import {useModal} from '@/hooks/use-modal-store'
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles
@@ -25,6 +26,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({server, role}: ServerHeaderProps) => {
+  const {onOpen} = useModal()
+
   const isAdmin = role === MemberRole.ADMIN
   const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -42,7 +45,10 @@ export const ServerHeader = ({server, role}: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]'>
         {isModerator && (
-          <DropdownMenuItem className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'
+            onClick={() => onOpen('invite', {server})}
+          >
             Invite People
             <UserPlus className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
