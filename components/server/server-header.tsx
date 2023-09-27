2cb98d3ceb86675a073c59925592563ec912079e
Vanessa <vanessa.riosm571@gmail.com>
2023-09-27 17:53:47 -0500

    feat: add 'manage members' option functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -65,7 +65,10 @@ export const ServerHeader = ({server, role}: ServerHeaderProps) => {
         )}
 
         {isAdmin && (
-          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            className='px-3 py-2 text-sm cursor-pointer'
+            onClick={() => onOpen('members', {server})}
+          >
             Manage Members
             <Users className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
