857976772a8c8989aa2df1f9f88e3249a44752be
Vanessa <vanessa.riosm571@gmail.com>
2023-09-27 14:33:36 -0500

    feat: add server header & its options

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -2,6 +2,7 @@ import {currentProfile} from '@/lib/current-profile'
 import {db} from '@/lib/db'
 import {ChannelType} from '@prisma/client'
 import {redirect} from 'next/navigation'
+import {ServerHeader} from './server-header'
 
 interface ServerSidebarProps {
   serverId: string
@@ -49,5 +50,13 @@ export const ServerSidebar = async ({serverId}: ServerSidebarProps) => {
 
   if (!server) return redirect('/')
 
-  return <div>Server Sidebar Component</div>
+  const role = server.members.find(
+    (member) => member.profileId === profile.id
+  )?.role
+
+  return (
+    <div className='flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]'>
+      <ServerHeader server={server} role={role} />
+    </div>
+  )
 }
