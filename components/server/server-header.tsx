b2f8f90314cfac03f02932c6f42562ede58d8b60
Vanessa <vanessa.riosm571@gmail.com>
2023-09-27 16:28:05 -0500

    feat: add 'server settings' option functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -55,7 +55,10 @@ export const ServerHeader = ({server, role}: ServerHeaderProps) => {
         )}
 
         {isAdmin && (
-          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            className='px-3 py-2 text-sm cursor-pointer'
+            onClick={() => onOpen('editServer', {server})}
+          >
             Server Settings
             <Settings className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
