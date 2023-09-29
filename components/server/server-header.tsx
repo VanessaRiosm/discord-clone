0b5ac050688f8b4edfd4a5b91643aabbf7ee30ef
Vanessa <vanessa.riosm571@gmail.com>
2023-09-29 13:46:21 -0500

    feat: add 'leave the server' option functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -94,7 +94,10 @@ export const ServerHeader = ({server, role}: ServerHeaderProps) => {
         )}
 
         {!isAdmin && (
-          <DropdownMenuItem className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+            onClick={() => onOpen('leaveServer', {server})}
+          >
             Leave Server
             <LogOut className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -7,6 +7,7 @@ export type ModalType =
   | 'editServer'
   | 'members'
   | 'createChannel'
+  | 'leaveServer'
 
 interface ModalData {
   server?: Server
