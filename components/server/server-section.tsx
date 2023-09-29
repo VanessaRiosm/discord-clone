cce5368f678cbd42120e6dc9877569eb3eb282f5
Vanessa <vanessa.riosm571@gmail.com>
2023-09-29 15:37:22 -0500

    feat: add search functionality

---
+++ components/server/server-section.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import {ServerWithMembersWithProfiles} from '@/types'
+import {ChannelType, MemberRole} from '@prisma/client'
+
+interface ServerSectionProps {
+  label: string
+  role?: MemberRole
+  sectionType: 'channels' | 'members'
+  channelType?: ChannelType
+  server?: ServerWithMembersWithProfiles
+}
+
+export const ServerSection = ({
+  label,
+  role,
+  sectionType,
+  channelType,
+  server,
+}: ServerSectionProps) => {
+  return <div>Server Section</div>
+}
