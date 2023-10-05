aeaaead7b167568db5b21542f75614eaf52a285d
Vanessa <vanessa.riosm571@gmail.com>
2023-10-05 11:53:02 -0500

    feat: add mobile-toggle

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,45 @@
-const ChannelIdPage = () => {
-  return <div>Channel ID Page!</div>
+import {currentProfile} from '@/lib/current-profile'
+import {redirectToSignIn} from '@clerk/nextjs'
+import {db} from '@/lib/db'
+import {redirect} from 'next/navigation'
+import {ChatHeader} from '@/components/chat/chat-header'
+
+interface ChannelIdProps {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+
+const ChannelIdPage = async ({params}: ChannelIdProps) => {
+  const profile = await currentProfile()
+
+  if (!profile) return redirectToSignIn()
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  })
+
+  if (!channel || !member) redirect('/')
+
+  return (
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type='channel'
+      />
+    </div>
+  )
 }
 
 export default ChannelIdPage
