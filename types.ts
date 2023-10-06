d98cb5bd18a537a9a0483e0a2fce9842380c1131
Vanessa <vanessa.riosm571@gmail.com>
2023-10-06 15:37:35 -0500

    feat: add socket provider

--- types.ts
+++ types.ts
@@ -1,5 +1,17 @@
+import {Server as NetServer, Socket} from 'net'
+import {NextApiResponse} from 'next'
+import {Server as SocketIOServer} from 'socket.io'
 import {Server, Member, Profile} from '@prisma/client'
 
 export type ServerWithMembersWithProfiles = Server & {
   members: (Member & {profile: Profile})[]
+}
+
+// custom response type to use in pages
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer
+    }
+  }
 }
