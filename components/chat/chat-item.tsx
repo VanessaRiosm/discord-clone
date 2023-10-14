a557eeb580784ab083e0adc34cf7230fa6ef6a7d
Vanessa <vanessa.riosm571@gmail.com>
2023-10-14 16:21:49 -0500

    feat: add edit option functionality

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -10,9 +10,11 @@ import {UserAvatar} from '@/components/user-avatar'
 import {ActionTooltip} from '@/components/action-tooltip'
 import {Edit, FileIcon, ShieldAlert, ShieldCheck, Trash} from 'lucide-react'
 import Image from 'next/image'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import {cn} from '@/lib/utils'
 import {Form, FormControl, FormField, FormItem} from '@/components/ui/form'
+import {Input} from '@/components/ui/input'
+import {Button} from '@/components/ui/button'
 
 interface ChatItemProps {
   id: string
@@ -35,6 +37,10 @@ const roleIconMap = {
   ADMIN: <ShieldAlert className='h-4 w-4 ml-2 text-rose-500' />,
 }
 
+const formSchema = z.object({
+  content: z.string().min(1),
+})
+
 export const ChatItem = ({
   id,
   content,
@@ -49,6 +55,40 @@ export const ChatItem = ({
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false)
   const [isDeleting, setIsDeleting] = useState(false)
+
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      if (event.key === 'Escape' || event.keyCode === 27) {
+        setIsEditing(false)
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [])
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: content,
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    try {
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    form.reset({
+      content: content,
+    })
+  }, [content])
 
   const fileType = fileUrl?.split('.').pop()
 
@@ -121,6 +161,39 @@ export const ChatItem = ({
                 <span className='text-[10px] mx-2 text-zinc-500 dark:text-zinc-400'></span>
               )}
             </p>
+          )}
+          {!fileUrl && isEditing && (
+            <Form {...form}>
+              <form
+                className='flex items-center w-full gap-x-2 pt-2'
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <FormField
+                  control={form.control}
+                  name='content'
+                  render={({field}) => (
+                    <FormItem className='flex-1'>
+                      <FormControl>
+                        <div className='relative w-full'>
+                          <Input
+                            disabled={isLoading}
+                            className='p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring:0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200'
+                            placeholder='Edited message'
+                            {...field}
+                          />
+                        </div>
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+                <Button disabled={isLoading} size='sm' variant='primary'>
+                  Save
+                </Button>
+              </form>
+              <span className='text-[10px] mt-1 text-zinc-400'>
+                Press escape to cancel, enter to save
+              </span>
+            </Form>
           )}
         </div>
       </div>
